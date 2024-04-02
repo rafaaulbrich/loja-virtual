47e3f4d9f336b593add095d81b162d8afa5507cf
rafaaulbrich <rafaelacamilly2018@gmail.com>
2024-04-02 11:30:43 -0300

    commit

--- src/layouts/LayoutSmall.vue
+++ src/layouts/LayoutSmall.vue
@@ -4,15 +4,13 @@ import SmallHeader from '@/components/templates/small/SmallHeader.vue'
 </script>
 
 <template>
-  <template>
-    <header>
-      <small-header />
-    </header>
-    <main>
-      <router-view />
-    </main>
-    <footer>
-      <small-footer />
-    </footer>
-  </template>
+  <header>
+    <small-header />
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer>
+    <small-footer />
+  </footer>
 </template>
